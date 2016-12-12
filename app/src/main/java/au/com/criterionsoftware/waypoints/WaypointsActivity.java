1661dbbe7766964f0d15834a007a2ed4b10a75b5
Darren Oster <darren.oster@gmail.com>
2016-12-12 23:02:40 +1030

    Waypoints displayed in list. No actions as yet (will have remove, move up, move to top, move down, move to end).

--- app/src/main/java/au/com/criterionsoftware/waypoints/WaypointsActivity.java
+++ app/src/main/java/au/com/criterionsoftware/waypoints/WaypointsActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -40,10 +44,25 @@ public class WaypointsActivity extends AppCompatActivity {
 			waypoints = new ArrayList<>();
 		}
 
+
+		ArrayList<String> sa = new ArrayList<>();
 		int i = 1;
 		for (LatLng latLng : waypoints) {
-			Log.d(LOG_TAG, String.format(Locale.getDefault(), "%d. %f, %f", i++, latLng.latitude, latLng.longitude));
+			sa.add(String.format(Locale.getDefault(), "%d. %f, %f", i++, latLng.latitude, latLng.longitude));
 		}
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sa);
+		ListView lv = (ListView)findViewById(R.id.list);
+		lv.setAdapter(adapter);
+		lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> a, View v, int position, long id)
+			{
+				Toast.makeText(getBaseContext(), "Click, position = " + position, Toast.LENGTH_LONG).show();
+			}
+		});
+
 	}
 
 	@Override
